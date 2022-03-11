ceda705793e23b1c3bca3a22dc9a4524b1f9c1ae
y.takahashi <97076013+pandapanda13@users.noreply.github.com>
2022-03-11 04:39:11 +0000

    [Update]top about

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,10 +1,10 @@
 class Public::HomesController < ApplicationController
- 
+
   def top
     @items = Item.order(created_at: :desc). limit(3)
     @item_comment_ranks = Item.find(Comment.group(:item_id).order('count(item_id) desc').pluck(:item_id))
-  end 
+  end
   def about
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
   scope module: 'public' do
     root to: 'homes#top'
     get 'about', to: 'homes#about'
+
   end
   namespace :admin do
     resources :orders, only:[:show, :update]
